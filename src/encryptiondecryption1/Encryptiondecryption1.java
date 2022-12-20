3d8f808eb6a8ce49c0fbc69e0afed1bdf5ee57d7
PC <PC@DESKTOP-26A9125>
2022-12-20 13:00:59 -0800

    initial commit

---
+++ src/encryptiondecryption1/Encryptiondecryption1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package encryptiondecryption1;
+
+/**
+ *
+ * @author PC
+ */
+public class Encryptiondecryption1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        project proj = new project();
+        // TODO code application logic here
+    }
+    
+}
